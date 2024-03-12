b5e483acec52a62a8f83f109029c9131b1199c4e
Suchitra Sahoo <suchitrasahoo874@gmail.com>
2024-03-12 23:27:53 +0530

    12/03/27

--- my-app/src/components/Video2.js
+++ my-app/src/components/Video2.js
@@ -1,15 +1,16 @@
-import img1 from './lady.jpg';
+import img1 from './lady.jpg'; // Import the image files
+import './video.css';
 import img2 from './woman.jpg';
+
 function Video2({ title, bgcolor }) {
-    return (
-      <div>
-        <div style={{ backgroundColor: bgcolor }}>video</div>
-        <div><img src={img1} alt="Lady" width="400" height="400" /></div>
-        <div style={{ backgroundColor: 'red' }}>{title}</div>
-        <div><img src={img2} alt="Woman" width="400" height="400" /></div>
-      </div>
-    );
-  }
-  export default Video2;
-  
-  
+  return (
+    <div>
+      <div style={{ backgroundColor: bgcolor }}>video</div>
+      <div className="pic"><img src={img1} alt="Lady" width="400" height="400" /></div>
+      <div style={{ backgroundColor: 'red' }}>{title}</div>
+      <div><img src={img2} alt="Woman" width="400" height="400" /></div>
+    </div>
+  );
+}
+
+export default Video2;
--- my-app/src/components/video.js
+++ my-app/src/components/video.js
@@ -5,7 +5,7 @@ function Video(props) {
   return (
     <div>
       <div style={{ backgroundColor: 'red' }}>video</div>
-      <div><img src={img1} alt="Lady" width="400" height="400" /></div>
+      <div className="blue-background"><img src={img1} alt="Lady" width="400" height="400" /></div>
       <div style={{ backgroundColor: 'red' }}>{props.title}</div>
       <div><img src={img2} alt="Woman" width="400" height="400" /></div>
     </div>
